ee3a66056cf280ecb5fb37e904c5d7491d557cd8
unknown <lakshmirajt02@gmail.com>
2026-07-10 11:17:53 +0530

    commit

---
+++ activity1.py
@@ -0,0 +1,87 @@
+# activity 1 - add a new fruit(list)
+# fruits = ['apple', 'banana', 'mango']
+# new_fruit = input('Enter a fruit: ')
+# fruits.append(new_fruit)
+# print(fruits)
+
+# activity 2 - remove a subject 
+# subjects = ['python', 'java', 'c++', 'javascript']
+# remove_subject = input("Enter subject to remove: ")
+# subjects.remove(remove_subject)
+# print(subjects)
+
+# activity 3 - find student position
+# students = ['asha', 'rahul', 'meera', 'john']
+# student_name = input("Enter student name: ")
+# print(students.index(student_name))
+
+# activity 4 - count a number 
+# numbers = [10, 20, 10, 30, 10, 40]
+# find_number = int(input('Enter number: '))
+# print(numbers.count(find_number))
+
+# activity 5 - sort marks
+# marks = [65, 82, 40, 95, 70]
+# marks.sort()
+# print(marks)
+
+# activity 6 - reverse a list 
+# colors = ['red', 'blue', 'green', 'yellow']
+# colors.reverse()
+# print(colors)
+
+# activity 7 - update a list item 
+# cities = ['delhi', 'mumbai', 'chennai']
+# index = int(input())
+# new_city = input('Enter new city: ')
+# cities[index] = new_city
+# print(cities)
+
+# activity 8 - create a tuple 
+# name = input('Enter name: ')
+# age =int(input("Enter age: "))
+# student = (name, age)
+# print(student)
+
+# activity 9 - access tuple elements
+# employee = ('john', 28, 'Developer')
+# print('name : ', employee[0])
+# print('age : ', employee[1])
+# print('role : ', employee[2])
+
+# activity 10 -count item in tuple 
+# numbers = (1, 2, 3, 2, 4, 2)
+# find_value = int(input("Enter the value to be counted: "))
+# count = numbers.count(find_value)
+# print(find_value, " appears ", count ," times" )
+
+# activity 11 - find index in tuple 
+# languages = ('python', 'java', 'C', 'javascript')
+# find_index = input('Enter language:')
+# print(languages.index(find_index))
+
+# activity 12 - convert tuple to list 
+# numbers = (10, 20, 30, 40)
+# list_item = list(numbers)
+# print(list_item)
+
+# activity 13 - list + tuple
+# subjects = ['python', 'java']
+# new_subjects = ('react', 'django')
+# list_subjects = list(new_subjects)
+# subjects.extend(list_subjects)
+# print(subjects)
+
+# activity 14 - maximum and minimum 
+# marks = [75 , 82 , 90, 65, 88]
+# print(min(marks))
+# print(max(marks))
+
+# activity 15 - List methods practice 
+numbers = [5, 10, 15]
+numbers[1] = 20
+numbers.append(7)
+numbers.sort(reverse = True)
+print(numbers)
+
+

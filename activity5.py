3ecdffda772b36da8b1a2742ba8f4220afdc8986
unknown <lakshmirajt02@gmail.com>
2026-08-02 23:39:41 +0530

    break

--- activity5.py
+++ activity5.py
@@ -7,15 +7,26 @@ for i in range(1,51):
 print(newlist)
 
 # Right-angle triangle pattern
-for i in range(6):
-   for j in range(i):
-      print("*", end=" ")
-   print()
+# for i in range(6):
+#    for j in range(i):
+#       print("*", end=" ")
+#    print()
    
 # reverse triangle pattern 
-for a in range(6):
-   for b in range(5,1):
+for a in range(5,0,-1):
+   for b in range(a):
       print("*", end= " ")
    print()
    
+#  Use of break
+for i in range(1,11):
+   print(i)
+   if i == 5:
+      break
+     
+#  Use of continue
+for j in range(1,6):
+   if j == 3:
+      continue
+   print(j)
 
